57e61b8923acca1915bca9295ebe876e4cb0a491
melodicore <datafox@datafox.me>
2023-09-16 23:07:31 +0300

    forgot to commit defaultFor exception

---
+++ injector/src/main/java/me/datafox/dfxengine/injector/exception/InvalidDefaultTypeException.java
@@ -0,0 +1,32 @@
+package me.datafox.dfxengine.injector.exception;
+
+import me.datafox.dfxengine.injector.api.annotation.Component;
+
+/**
+ * An exception that is thrown when a {@link Component} has a defaultFor value that is not a superclass or -interface of
+ * the component class.
+ *
+ * @author datafox
+ */
+public class InvalidDefaultTypeException extends RuntimeException {
+    public InvalidDefaultTypeException() {
+        super();
+    }
+
+    public InvalidDefaultTypeException(String message) {
+        super(message);
+    }
+
+    public InvalidDefaultTypeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidDefaultTypeException(Throwable cause) {
+        super(cause);
+    }
+
+    protected InvalidDefaultTypeException(String message, Throwable cause,
+                                          boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
