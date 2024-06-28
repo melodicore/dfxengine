79c258e47f726b987d7a095e0f573629a5ef45b8
melodicore <datafox@datafox.me>
2024-06-28 11:22:30 +0300

    Add Named interface

---
+++ text-api/src/main/java/me/datafox/dfxengine/text/api/Named.java
@@ -0,0 +1,16 @@
+package me.datafox.dfxengine.text.api;
+
+/**
+ * An interface that signals that the implementing class has a name in singular and plural form. {@link T} must be the
+ * class of the implementing class or its superclass or superinterface. Classes implementing this interface will have
+ * priority over {@link NameConverter NameConverters}, but not the names explicitly configured with the
+ * {@link TextFactory}.
+ *
+ * @author datafox
+ */
+public interface Named<T> {
+    /**
+     * @return {@link Name of this object}
+     */
+    Name<T> getName();
+}
