96a0e6e317702860b659bf5e7fdf5b1982698004
melodicore <datafox@datafox.me>
2024-07-16 21:11:24 +0300

    forgot to remove temporary println calls

--- injector/src/main/java/me/datafox/dfxengine/injector/internal/ComponentDataFactory.java
+++ injector/src/main/java/me/datafox/dfxengine/injector/internal/ComponentDataFactory.java
@@ -194,9 +194,7 @@ public class ComponentDataFactory {
     }
 
     private String parseSuperclass(String str) {
-        System.out.println("AAA: " + str);
         str = InjectorUtils.splitWithoutTypes(str, " extends ", 2)[1];
-        System.out.println("AAA: " + str);
         return toFullSignature(str);
     }
 
