4689f72a71a45c9fc4cb0b2531e1554ba9b67dfa
melodicore <datafox@datafox.me>
2024-07-24 14:28:11 +0300

    Use Class.getName() for EntityDataTypes.class keys

--- entities-api/src/main/java/me/datafox/dfxengine/entities/api/DataType.java
+++ entities-api/src/main/java/me/datafox/dfxengine/entities/api/DataType.java
@@ -9,10 +9,6 @@ import lombok.Data;
 public final class DataType<T> {
     private final Class<T> type;
 
-    public String getId() {
-        return type.getName();
-    }
-
     public static <T> DataType<T> of(Class<T> type) {
         return new DataType<>(type);
     }
--- entities/src/main/java/me/datafox/dfxengine/entities/utils/EntityDataTypes.java
+++ entities/src/main/java/me/datafox/dfxengine/entities/utils/EntityDataTypes.java
@@ -3,11 +3,12 @@ package me.datafox.dfxengine.entities.utils;
 import me.datafox.dfxengine.entities.api.DataType;
 import me.datafox.dfxengine.injector.api.annotation.Component;
 import me.datafox.dfxengine.injector.api.annotation.Inject;
+import me.datafox.dfxengine.values.api.Value;
+import me.datafox.dfxengine.values.api.ValueMap;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * @author datafox
@@ -18,15 +19,28 @@ public class EntityDataTypes {
 
     @Inject
     public EntityDataTypes(List<DataType<?>> dataTypes) {
-        types = new HashMap<>(dataTypes
-                .stream()
-                .collect(Collectors.toMap(
-                        DataType::getId,
-                        DataType::getType)));
+        types = new HashMap<>();
+        dataTypes.stream()
+                .map(DataType::getType)
+                .forEach(EntityDataTypes::registerDataType);
+    }
+
+    public static void registerDataType(Class<?> type) {
+        types.put(type.getName(), type);
     }
 
     @SuppressWarnings("unchecked")
     public static <T> Class<T> getType(String id) {
         return (Class<T>) types.get(id);
+    }
+
+    @Component
+    private static DataType<Value> valueType() {
+        return DataType.of(Value.class);
+    }
+
+    @Component
+    private static DataType<ValueMap> valueMapType() {
+        return DataType.of(ValueMap.class);
     }
 }
