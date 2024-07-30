464b706b3d4742f73c0b426fbc0c07d831235bc5
melodicore <datafox@datafox.me>
2024-07-30 15:54:39 +0300

    fix value map data state loading

--- entities/src/main/java/me/datafox/dfxengine/entities/data/ValueMapData.java
+++ entities/src/main/java/me/datafox/dfxengine/entities/data/ValueMapData.java
@@ -4,7 +4,6 @@ import me.datafox.dfxengine.entities.api.state.DataState;
 import me.datafox.dfxengine.entities.definition.data.ValueMapDataDefinition;
 import me.datafox.dfxengine.entities.state.ValueMapState;
 import me.datafox.dfxengine.entities.utils.EntityHandles;
-import me.datafox.dfxengine.entities.utils.internal.EntityUtils;
 import me.datafox.dfxengine.handles.TreeHandleMap;
 import me.datafox.dfxengine.handles.api.Space;
 import me.datafox.dfxengine.values.DelegatedValueMap;
@@ -29,12 +28,10 @@ public class ValueMapData extends AbstractStatefulData<ValueMap> {
         ValueMap map = new DelegatedValueMap(
                 new TreeHandleMap<>(s, logger),
                 false, logger);
-        map.set(definition
-                .getValues()
+        definition.getValues()
                 .stream()
-                .collect(Collectors.toMap(
-                        value -> EntityUtils.getValueHandle(s, value),
-                        EntityUtils::getValueNumeral)));
+                .map(v -> v.buildValue(s, false))
+                .forEach(map::putHandled);
         return map;
     }
 
@@ -43,12 +40,10 @@ public class ValueMapData extends AbstractStatefulData<ValueMap> {
         ValueMapState cast = castState(state, ValueMapState.class);
         Space space = getData().getSpace();
         getData().clear();
-        getData().set(cast
-                .getValues()
+        cast.getValues()
                 .stream()
-                .collect(Collectors.toMap(
-                        value -> EntityUtils.getValueHandle(space, value),
-                        EntityUtils::getValueNumeral)));
+                .map(v -> v.buildValue(space, false))
+                .forEach(getData()::putHandled);
     }
 
     @Override
